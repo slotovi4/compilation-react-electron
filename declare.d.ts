1d921074581d28fec4275457014250fd799db1a0
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-09-01 16:08:00 +0300

    init

--- declare.d.ts
+++ declare.d.ts
@@ -1,3 +1,7 @@
+/// <reference types="node" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
 declare module '*.svg'
 declare module '*.png'
 declare module '*.jpg'
---
+++ src/components/Reservation/Reservation.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const Reservation = () => {
+    console.log(213);
+    return (
+        <div>
+            <span>123</span>
+        </div>
+    );
+};
+
+export default Reservation;
